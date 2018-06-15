877f48c6ad438161e8f6b83c211b7cd5f0f6d33c
brianhhu <brianh@alleninstitute.org>
2018-06-15 11:29:03 -0700

    Initial commit of DOC and HED models

---
+++ hed.py
@@ -0,0 +1,182 @@
+import tensorflow as tf
+
+__weights_dict = dict()
+
+is_train = False
+
+
+def load_weights(weight_file):
+    import numpy as np
+
+    if weight_file == None:
+        return
+
+    try:
+        weights_dict = np.load(weight_file).item()
+    except:
+        weights_dict = np.load(weight_file, encoding='bytes').item()
+
+    return weights_dict
+
+
+def KitModel(weight_file=None):
+    global __weights_dict
+    __weights_dict = load_weights(weight_file)
+
+    data = tf.placeholder(tf.float32, shape=(None, None, None, 3), name='data')
+
+    # conv1
+    conv1_1_pad = tf.pad(data, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv1_1 = convolution(conv1_1_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv1_1')
+    relu1_1 = tf.nn.relu(conv1_1, name='relu1_1')
+    conv1_2_pad = tf.pad(relu1_1, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv1_2 = convolution(conv1_2_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv1_2')
+    relu1_2 = tf.nn.relu(conv1_2, name='relu1_2')
+    pool1_pad = tf.pad(relu1_2, paddings=[[0, 0], [0, 1], [0, 1], [
+                       0, 0]], constant_values=float('-Inf'))
+    pool1 = tf.nn.max_pool(pool1_pad, [1, 2, 2, 1], [
+                           1, 2, 2, 1], padding='VALID', name='pool1')
+
+    # conv2
+    conv2_1_pad = tf.pad(pool1, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv2_1 = convolution(conv2_1_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv2_1')
+    relu2_1 = tf.nn.relu(conv2_1, name='relu2_1')
+    conv2_2_pad = tf.pad(relu2_1, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv2_2 = convolution(conv2_2_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv2_2')
+    relu2_2 = tf.nn.relu(conv2_2, name='relu2_2')
+    pool2_pad = tf.pad(relu2_2, paddings=[[0, 0], [0, 1], [0, 1], [
+                       0, 0]], constant_values=float('-Inf'))
+    pool2 = tf.nn.max_pool(pool2_pad, [1, 2, 2, 1], [
+                           1, 2, 2, 1], padding='VALID', name='pool2')
+
+    # conv3
+    conv3_1_pad = tf.pad(pool2, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv3_1 = convolution(conv3_1_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv3_1')
+    relu3_1 = tf.nn.relu(conv3_1, name='relu3_1')
+    conv3_2_pad = tf.pad(relu3_1, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv3_2 = convolution(conv3_2_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv3_2')
+    relu3_2 = tf.nn.relu(conv3_2, name='relu3_2')
+    conv3_3_pad = tf.pad(relu3_2, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv3_3 = convolution(conv3_3_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv3_3')
+    relu3_3 = tf.nn.relu(conv3_3, name='relu3_3')
+    pool3_pad = tf.pad(relu3_3, paddings=[[0, 0], [0, 1], [0, 1], [
+                       0, 0]], constant_values=float('-Inf'))
+    pool3 = tf.nn.max_pool(pool3_pad, [1, 2, 2, 1], [
+                           1, 2, 2, 1], padding='VALID', name='pool3')
+
+    # conv4
+    conv4_1_pad = tf.pad(pool3, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv4_1 = convolution(conv4_1_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv4_1')
+    relu4_1 = tf.nn.relu(conv4_1, name='relu4_1')
+    conv4_2_pad = tf.pad(relu4_1, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv4_2 = convolution(conv4_2_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv4_2')
+    relu4_2 = tf.nn.relu(conv4_2, name='relu4_2')
+    conv4_3_pad = tf.pad(relu4_2, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv4_3 = convolution(conv4_3_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv4_3')
+    relu4_3 = tf.nn.relu(conv4_3, name='relu4_3')
+    pool4_pad = tf.pad(relu4_3, paddings=[[0, 0], [0, 1], [0, 1], [
+                       0, 0]], constant_values=float('-Inf'))
+    pool4 = tf.nn.max_pool(pool4_pad, [1, 2, 2, 1], [
+                           1, 2, 2, 1], padding='VALID', name='pool4')
+
+    # conv5
+    conv5_1_pad = tf.pad(pool4, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv5_1 = convolution(conv5_1_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv5_1')
+    relu5_1 = tf.nn.relu(conv5_1, name='relu5_1')
+    conv5_2_pad = tf.pad(relu5_1, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv5_2 = convolution(conv5_2_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv5_2')
+    relu5_2 = tf.nn.relu(conv5_2, name='relu5_2')
+    conv5_3_pad = tf.pad(relu5_2, paddings=[[0, 0], [1, 1], [1, 1], [0, 0]])
+    conv5_3 = convolution(conv5_3_pad, group=1, strides=[
+                          1, 1], padding='VALID', name='conv5_3')
+    relu5_3 = tf.nn.relu(conv5_3, name='relu5_3')
+
+    # conv1-side
+    score_dsn1 = convolution(relu1_2, group=1, strides=[
+                             1, 1], padding='VALID', name='score-dsn1')
+    sigmoid_dsn1 = tf.sigmoid(score_dsn1, name='sigmoid-dsn1')
+
+    # conv2-side
+    score_dsn2 = convolution(relu2_2, group=1, strides=[
+                             1, 1], padding='VALID', name='score-dsn2')
+    upsample_2 = convolution_transpose(score_dsn2, output_shape=[1, tf.shape(data)[1], tf.shape(data)[2], 1], strides=[
+                                       1, 2, 2, 1], padding='SAME', name='upsample_2')
+    sigmoid_dsn2 = tf.sigmoid(upsample_2, name='sigmoid-dsn2')
+
+    # conv3-side
+    score_dsn3 = convolution(relu3_3, group=1, strides=[
+                             1, 1], padding='VALID', name='score-dsn3')
+    upsample_4 = convolution_transpose(score_dsn3, output_shape=[1, tf.shape(data)[1], tf.shape(data)[2], 1], strides=[
+                                       1, 4, 4, 1], padding='SAME', name='upsample_4')
+    sigmoid_dsn3 = tf.sigmoid(upsample_4, name='sigmoid-dsn3')
+
+    # conv4-side
+    score_dsn4 = convolution(relu4_3, group=1, strides=[
+                             1, 1], padding='VALID', name='score-dsn4')
+    upsample_8 = convolution_transpose(score_dsn4, output_shape=[1, tf.shape(data)[1], tf.shape(data)[2], 1], strides=[
+                                       1, 8, 8, 1], padding='SAME', name='upsample_8')
+    sigmoid_dsn4 = tf.sigmoid(upsample_8, name='sigmoid-dsn4')
+
+    # conv5-side
+    score_dsn5 = convolution(relu5_3, group=1, strides=[
+                             1, 1], padding='VALID', name='score-dsn5')
+    upsample_16 = convolution_transpose(score_dsn5, output_shape=[1, tf.shape(data)[1], tf.shape(data)[2], 1], strides=[
+                                        1, 16, 16, 1], padding='SAME', name='upsample_16')
+    sigmoid_dsn5 = tf.sigmoid(upsample_16, name='sigmoid-dsn5')
+
+    # concatenate
+    concat = tf.concat([score_dsn1, upsample_2, upsample_4,
+                        upsample_8, upsample_16], 3, name='concat')
+    new_score_weighting = convolution(concat, group=1, strides=[
+                                      1, 1], padding='VALID', name='new-score-weighting')
+    sigmoid_fuse = tf.sigmoid(new_score_weighting, name='sigmoid-fuse')
+    return data, (sigmoid_dsn1, sigmoid_dsn2, sigmoid_dsn3, sigmoid_dsn4, sigmoid_dsn5, sigmoid_fuse)
+
+
+def convolution_transpose(input, name, **kwargs):
+    w = tf.Variable(__weights_dict[name]['weights'],
+                    trainable=is_train, name=name + "_weight")
+    dim = __weights_dict[name]['weights'].ndim - 2
+    if dim == 2:
+        layer = tf.nn.conv2d_transpose(input, w, **kwargs)
+    elif dim == 3:
+        layer = tf.nn.conv3d_transpose(input, w, **kwargs)
+    else:
+        raise ValueError("Error dim number {} in ConvTranspose".format(dim))
+
+    if 'bias' in __weights_dict[name]:
+        b = tf.Variable(__weights_dict[name]['bias'],
+                        trainable=is_train, name=name + "_bias")
+        layer = layer + b
+    return layer
+
+
+def convolution(input, name, group, **kwargs):
+    w = tf.Variable(__weights_dict[name]['weights'],
+                    trainable=is_train, name=name + "_weight")
+    if group == 1:
+        layer = tf.nn.convolution(input, w, **kwargs)
+    else:
+        weight_groups = tf.split(w, num_or_size_splits=group, axis=-1)
+        xs = tf.split(input, num_or_size_splits=group, axis=-1)
+        convolved = [tf.nn.convolution(x, weight, **kwargs) for
+                     (x, weight) in zip(xs, weight_groups)]
+        layer = tf.concat(convolved, axis=-1)
+
+    if 'bias' in __weights_dict[name]:
+        b = tf.Variable(__weights_dict[name]['bias'],
+                        trainable=is_train, name=name + "_bias")
+        layer = layer + b
+    return layer
---
+++ tensorflow_inference.py
@@ -0,0 +1,49 @@
+# inference with tensorflow
+from __future__ import absolute_import
+import argparse
+import numpy as np
+from six import text_type as _text_type
+from tensorflow.python.keras.preprocessing import image
+import tensorflow as tf
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-n', type=_text_type, default='kit_doc',
+                    help='Network structure file name.')
+
+
+parser.add_argument('-w', type=_text_type, required=True,
+                    help='Network weights file name')
+
+parser.add_argument('--image', '-i',
+                    type=_text_type, help='Test image path.',
+                    default="41004.jpg"
+                    )
+
+
+args = parser.parse_args()
+if args.n.endswith('.py'):
+    args.n = args.n[:-3]
+
+# import converted model
+model_converted = __import__(args.n).KitModel(args.w)
+input_tf, model_tf = model_converted
+
+# load img with BGRTranspose=True
+img = image.load_img(args.image)
+img = image.img_to_array(img)
+img = img[..., ::-1]  # needed?
+
+input_data = np.expand_dims(img, 0)
+
+# inference with tensorflow
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    predict = sess.run(model_tf, feed_dict={input_tf: input_data})
+# print(predict)
+
+import matplotlib.pyplot as plt
+plt.imshow(predict[-1].squeeze())
+plt.show()
